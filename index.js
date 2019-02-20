594ccd89dc30f10cceacce660ac0be808461c421
sho kisaragi <sho@oshiire.to>
2019-02-20 16:34:22 +0900

    fix port number for Heroku

--- index.js
+++ index.js
@@ -68,4 +68,4 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('starting with port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('starting with port 3000'));
